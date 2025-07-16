174537017e86092164f31f5a14836d68c7dacf39
prohustler <prohustler100@gmail.com>
2025-07-16 23:42:03 +0900

    수정

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // 이 설정을 추가하여 정적 HTML로 결과물을 생성합니다.
   output: 'export',
-
-  // 정적 내보내기 시 next/image 최적화 기능을 사용하지 않도록 설정합니다.
+  trailingSlash: true,
   images: {
-    unoptimized: true,
+    unoptimized: true
   },
-};
+  // 기본 경로 설정 (필요한 경우)
+  // basePath: '',
+  // assetPrefix: '',
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig

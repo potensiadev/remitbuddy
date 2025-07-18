aca2bb96de61d356f7bc6f61f7ceda7558ffefd8
prohustler <prohustler100@gmail.com>
2025-07-19 04:38:25 +0900

    언어추가

--- frontend/next-i18next.config.js
+++ frontend/next-i18next.config.js
@@ -1,7 +1,20 @@
 module.exports = {
   i18n: {
     defaultLocale: 'en',
-    locales: ['en', 'vi', 'ko'],
+    locales: [
+      'en',  // English (default)
+      'ko',  // Korean 
+      'vi',  // Vietnamese
+      'tl',  // Filipino/Tagalog (Philippines)
+      'km',  // Khmer (Cambodia)
+      'my',  // Burmese (Myanmar)
+      'th',  // Thai (Thailand)
+      'uz',  // Uzbek (Uzbekistan)
+      'id',  // Indonesian (Indonesia)
+      'si',  // Sinhala (Sri Lanka)
+      'bn',  // Bengali (Bangladesh)
+      'ne',  // Nepali (Nepal)
+    ],
     localeDetection: false,
   },
   react: {

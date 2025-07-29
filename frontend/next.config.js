f35eeebb4365fa2a2089663166bc01d2f312fbf5
unknown <prohustler100@gmail.com>
2025-07-29 12:42:03 +0900

    dsf

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,11 +2,17 @@ const { i18n } = require('./next-i18next.config');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   trailingSlash: true,
   images: {
     unoptimized: true
   },
   i18n,
+  experimental: {
+    esmExternals: false
+  },
+  // Netlify 배포를 위한 설정
+  distDir: '.next',
 }
 
 module.exports = nextConfig
